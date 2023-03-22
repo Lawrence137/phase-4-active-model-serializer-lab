8d654f1f131f2ff6f3ff9d8b84465c08e4ef4c06
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-22 12:55:33 +0300

    serialization master

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,7 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name
+
+  has_one :profile
+  has_many :posts
+  has_many :tags
+end
---
+++ app/serializers/short_content_serializer.rb
@@ -0,0 +1,7 @@
+# class ShortContentSerializer < ActiveModel::Serializer
+#     attributes :short_content
+
+#     def short_content
+#         "#{self.object.content[0..39]}..."
+#       end
+# end
